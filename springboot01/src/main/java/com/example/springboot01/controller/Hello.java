83d779c3db8a73befb74fdaeda90da234bdf16e9
JuneJH <15310487388@163.com>
2022-07-19 08:06:13 +0800

    feat(*): springboot 多环境

--- springboot01/src/main/java/com/example/springboot01/controller/Hello.java
+++ springboot01/src/main/java/com/example/springboot01/controller/Hello.java
@@ -1,13 +1,17 @@
 package com.example.springboot01.controller;
 
+import com.example.springboot01.service.Send;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
+    @Autowired
+    private Send send;
 
     @GetMapping("/hello")
     public String say(){
-        return  "Hello Spring Boot!!!";
+        return  send.send();
     }
 }
